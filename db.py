030dda62add563cbe2de4bab51554106a2baf9d5
Hoorya Rafiq <hooryarafiq@gmail.com>
2024-02-09 05:13:00 -0500

    initial commit

---
+++ db.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+
+
+    # 1: {
+    #     "name": "Chair",
+    #     "price": 17.99
+    # },
+    # 2: {
+    #     "name": "Table",
+    #     "price": 180.59
+    # }
