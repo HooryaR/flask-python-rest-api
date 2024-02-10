642b0d3267a697e3c3d935abb38136ae86f4722c
Hoorya Rafiq <hooryarafiq@gmail.com>
2024-02-09 22:08:58 -0500

    Add task queues with rq and sending HTMl emails using Mailgun and Python

--- schemas.py
+++ schemas.py
@@ -42,3 +42,6 @@ class UserSchema(Schema):
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
     password = fields.Str(required=True) #Never send password to client
+
+class UserRegisterSchema(UserSchema):
+    email = fields.Str(required=True)
